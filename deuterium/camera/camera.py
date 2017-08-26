fe72cb8e5c64119bab330bcbae4e7a4cd373d185
TheMab <TheMab@users.noreply.github.com>
2017-08-26 19:34:12 +0100

    working user specified paths using sessions and setPath method

--- deuterium/camera/camera.py
+++ deuterium/camera/camera.py
@@ -1,19 +1,33 @@
 import cv2
 from base_camera import BaseCamera
 import imutils
-
+import os
+from flask import session
 
 class Camera(BaseCamera):
+
+
+    pathToVideo = 0
+
+    @staticmethod
+    def setPath(source):
+            Camera.pathToVideo= source
+
+
+
     @staticmethod
     def frames():
-        camera = cv2.VideoCapture(0)
+        camera = cv2.VideoCapture(Camera.pathToVideo)
+
         if not camera.isOpened():
             raise RuntimeError('Could not start camera.')
 
         while True:
             # read current frame
             _, img = camera.read()
+
             img = imutils.resize(img, 600)
 
             # encode as a jpeg image and return it
             yield cv2.imencode('.jpg', img)[1].tobytes()
+
--- deuterium/deuterium.py
+++ deuterium/deuterium.py
@@ -2,7 +2,7 @@
 """Main module."""
 
 # import necessary packages
-from flask import Flask, render_template, Response, request, session
+from flask import Flask, render_template, Response, request, session, jsonify
 from flask_bootstrap import Bootstrap
 from forms import pathToVideo
 from camera.camera import  Camera
@@ -13,13 +13,18 @@ Bootstrap(app)
 app.secret_key = os.urandom(24)
 
 
-@app.route('/')
+@app.route('/', methods=['GET','POST'])
 def index():
     form = pathToVideo()
+
+    # session['firstPath'] = "/resources/RightCam.mp4"
+
+
     return render_template('index.html', form=form)
 
 
 def gen(camera):
+    # Camera.setPath('/Users/mabs/Desktop/deuterium/deuterium/resources/RightCam.mp4')
     while True:
         frame = camera.get_frame()
         yield (b'--frame\r\n'
@@ -28,18 +33,22 @@ def gen(camera):
 
 @app.route('/video_feed')
 def video_feed():
+
+    Camera.setPath(session['firstPath'])
+
     """Video streaming route. Put this in the src attribute of an img tag."""
     return Response(gen(Camera()),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
-# @app.route('/result', methods=['GET','POST'])
-# def result():
-#     form = pathToVideo()
-#
-#     session['firstPath'] = form.firstPath.data
-#     session['secondPath'] = form.secondPath.data
-#     return render_template('result.html')
+
+@app.route('/result', methods=['POST'])
+def result():
+    form = pathToVideo()
+
+    session['firstPath'] = form.firstPath.data
+    session['secondPath'] = form.secondPath.data
+    return render_template('result.html')
 
 if __name__ == '__main__':
     app.config['TEMPLATES_AUTO_RELOAD'] = True
