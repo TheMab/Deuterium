214f980bde2d020382c45b1beb4ee4122ed152e2
TheMab <TheMab@users.noreply.github.com>
2017-09-10 14:43:58 +0100

    proj restructure

--- deuterium/detection/detection_algorithms/colour_detection.py
+++ deuterium/detection/detection_algorithms/colour_detection.py
@@ -11,18 +11,18 @@ class Colour_detector(Detector):
         blurred = cv2.GaussianBlur(frame, (11, 11), 0)
         hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
+
         mask = cv2.inRange(hsv, greenLower, greenUpper)
         mask = cv2.erode(mask, None, iterations=2)
         mask = cv2.dilate(mask, None, iterations=2)
+        mask = cv2.Canny(mask, 100, 200)
 
         cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
-        center = None
 
         # only proceed if at least one contour was found
         if len(cnts) > 0:
             # find the largest contour in the mask, then use
-            # it to compute the minimum enclosing circle and
-            # centroid
+            # it to compute the minimum enclosing circle
             c = max(cnts, key=cv2.contourArea)
             ((x, y), radius) = cv2.minEnclosingCircle(c)
 
